0be684e2edfa6bd3bfc5d456cd385bfcce4418d2
Gich-M <gichmuriuki21@gmail.com>
2024-10-20 14:32:12 +0300

    Variable Annotations and Type Checking

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Module sum_mixed_list."""
-from typing import Union
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Takes a list of integers and floats, and returns their sum.
 
