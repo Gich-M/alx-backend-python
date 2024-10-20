96b4d23432dd35079a9c88eba40e274c082947cc
Gich-M <gichmuriuki21@gmail.com>
2024-10-20 14:33:40 +0300

    Variable Annotations and Type Checking

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,15 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples, where each tuple contains
+        a sequence and its length.
+
+    Args:
+        lst: A sequence of sequences
+    Return:
+        A list of tuples with each tuple containing a sequence
+        and its length
+    """
+
     return [(i, len(i)) for i in lst]
