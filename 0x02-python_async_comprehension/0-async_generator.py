62ce173107b90df3c886e6b3e3d3882e7aca7ff4
Gich-M <gichmuriuki21@gmail.com>
2024-10-26 13:04:03 +0300

    Python async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> float:
     """
     Yields 10 random numbers between 0 and 10,
     with a 1-second delay between each yield.
