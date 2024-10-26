9614cac719d8fae9a5e4e109291ba380fd6388b1
Gich-M <gichmuriuki21@gmail.com>
2024-10-26 13:02:11 +0300

    Python async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module 1-async_comprehension"""
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """Collects 10 random numbers using async comprehension"""
+    return [num async for num in async_generator()]
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module 2-measure_runtime."""
+
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Executes async_comprehension 4 times in parallel and measures total runtime
+    Return:
+        Total runtime in seconds
+    """
+    start = time.perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    return time.perf_counter() - start
